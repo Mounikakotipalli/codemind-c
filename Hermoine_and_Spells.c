35d9447a8e55dceb393ac8d761cd61cee119027a
Mounikakotipalli <119840026+Mounikakotipalli@users.noreply.github.com>
2022-12-07 10:29:57 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a>b)     {         if(c>b)         {             printf("%d",a+c);         }         else         {             printf("%d",a+b);         }     }     else     {        if(c>a)        {            printf("%d",b+c);        }        else        {            printf("%d",a+b);        }     }
    }

---
+++ Hermoine_and_Spells.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {
+        if(c>b)
+        {
+            printf("%d",a+c);
+        }
+        else 
+        {
+            printf("%d",a+b);
+        }
+    }
+    else
+    {
+       if(c>a)
+       {
+           printf("%d",b+c);
+       }
+       else
+       {
+           printf("%d",a+b);
+       }
+    }
+    
+}
