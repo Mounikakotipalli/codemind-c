eb83bd4826c71efe98dd8e90508640f28c6754c8
Mounikakotipalli <119840026+Mounikakotipalli@users.noreply.github.com>
2022-12-23 20:14:48 +0530

    #include<stdio.h> int main() {     int p1,p2,p3,p4;     scanf("%d%d%d%d",&p1,&p2,&p3,&p4);     if(p1>10&&p2>10)     {         if(p3>10&&p4>=10)         {             printf("YES");         }         else         {             printf("NO");         }     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int p1,p2,p3,p4;
+    scanf("%d%d%d%d",&p1,&p2,&p3,&p4);
+    if(p1>10&&p2>10)
+    {
+        if(p3>10&&p4>=10)
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
+    }
+    else
+    {
+        printf("NO");
+    }
+}
