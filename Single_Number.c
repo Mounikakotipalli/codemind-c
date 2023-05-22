41182f23a342df831a35538e71e091526b047cfb
Mounikakotipalli <119840026+Mounikakotipalli@users.noreply.github.com>
2023-05-22 15:30:52 +0530

    #include<stdio.h> int main() {     int n,i,j,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j] )             {                 c++;                 }
            } 
                if(c==1) 
                { 
                    printf("%d",arr[i]); 
                    
                } 
            
        } 
        
    }

--- Single_Number.c
+++ Single_Number.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 int main()
-{
-    int n,i,j,c=0;
+{ 
+    int n,i,j,c=0; 
     scanf("%d",&n); 
     int arr[n]; 
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++) 
+    { 
         scanf("%d",&arr[i]);
-        }
-        for(i=0;i<n;i++) 
-        {
-            c=0; 
-            for(j=0;j<n;j++)
+    } 
+    for(i=0;i<n;i++) 
+    { 
+        c=0; 
+        for(j=0;j<n;j++) 
+        { 
+            if(arr[i]==arr[j] ) 
             {
-                if(arr[i]==arr[j] )
-                {
-                    c++; 
-                    
+                c++;
                 } 
+            
+        } 
+            if(c==1) 
+            { 
+                printf("%d",arr[i]); 
                 
             } 
-            if(c==1) 
-            {
-                printf("%d",arr[i]);
-                }
-                }
-                }
+        
+    } 
+    
+}
