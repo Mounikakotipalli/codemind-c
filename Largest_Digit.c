0eb2ec91632fdf09b3a3a9d4d13635d9ca70668d
Mounikakotipalli <119840026+Mounikakotipalli@users.noreply.github.com>
2023-06-04 07:32:22 +0530

    #include <stdio.h>
      int main() {
            int num, large = 0, rem = 0;
            scanf("%d", &num);
    
            /* finding the largest digit of the given input */
            while (num > 0) {
                    rem = num % 10;
    
                    if (rem > large) {
                            large = rem;
                    }
    
                    num = num / 10;
            }
    
            /* print the largest digit of the number */
            printf("%d",large);
    
      }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
-int main() {int num, large = 0, rem = 0;scanf("%d", &num);while (num > 0) {rem = num % 10;if (rem > large) {large = rem;}num = num / 10;}printf( "%d
-", large);return 0;}
+
+  int main() {
+        int num, large = 0, rem = 0;
+        scanf("%d", &num);
+
+        /* finding the largest digit of the given input */
+        while (num > 0) {
+                rem = num % 10;
+
+                if (rem > large) {
+                        large = rem;
+                }
+
+                num = num / 10;
+        }
+
+        /* print the largest digit of the number */
+        printf("%d",large);
+
+  }
